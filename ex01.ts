fa43762db1260bfdf5904cff678785b33f37a6c0
Nao Mausservey <nao.mausservey@efrei.net>
2021-10-04 09:29:27 +0200

    ex01

---
+++ ex01.ts
@@ -0,0 +1,41 @@
+const letter: any = {
+    'A': 'a',
+    'B': 'b',
+    'C': 'c',
+    'D': 'd',
+    'E': 'e',
+    'F': 'f',
+    'G': 'g',
+    'H': 'h',
+    'I': 'i',
+    'J': 'j',
+    'K': 'k',
+    'L': 'L',
+    'M': 'm',
+    'N': 'n',
+    'O': 'o',
+    'P': 'p',
+    'Q': 'q',
+    'R': 'r',
+    'S': 's',
+    'T': 't',
+    'U': 'u',
+    'V': 'v',
+    'W': 'w',
+    'X': 'x',
+    'Y': 'y',
+    'Z': 'z',
+  }
+
+
+function lowerCase(str:string){
+    let low=''
+    for (var i=0; i<str.length; i++){
+        low=low.concat(letter[str[i]])
+    }
+    return low
+
+}
+
+
+console.log(lowerCase('COUCOU'))
