ece08455c7c20aa238459844cde955469394c965
Nao Mausservey <nao.mausservey@efrei.net>
2021-10-04 09:34:58 +0200

    ex02

---
+++ ex02.ts
@@ -0,0 +1,40 @@
+const LETTER: any = {
+    'a': 'A',
+    'b': 'B',
+    'c': 'C',
+    'd': 'D',
+    'e': 'E',
+    'f': 'F',
+    'g': 'G',
+    'h': 'H',
+    'i': 'I',
+    'j': 'J',
+    'k': 'K',
+    'l': 'L',
+    'm': 'M',
+    'n': 'N',
+    'o': 'O',
+    'p': 'P',
+    'q': 'Q',
+    'r': 'R',
+    's': 'S',
+    't': 'T',
+    'u': 'U',
+    'v': 'V',
+    'w': 'W',
+    'x': 'X',
+    'y': 'Y',
+    'z': 'Z',
+  }
+
+
+function upperCase(stri:string){
+    let upper=''
+    for (var x=0; x<stri.length; x++){
+        upper=upper.concat(LETTER[stri[x]])
+    }
+    return upper
+
+}
+
+console.log(upperCase('coucou'))
